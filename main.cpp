dab15bdcc0a4fabb877c6fd7dc5aa5f4feac691e
gvenkatacharan <venkatacharan.g16@iiits.in>
2021-06-29 18:30:41 +0530

    removed function and fuction3

--- main.cpp
+++ main.cpp
@@ -1,20 +1,8 @@
 #include <stdio.h>
 
-void funciton(int x)
-{
-    cout << "funciton::you have provoded the interger:" << x << "\n";
-    cout << "function::added a line here" << "\n";
-}
-
-
 void funciton2(int x)
 {
     cout << "funciton::you have provoded the interger:" << x << "\n";
-}
-
-void function3()
-{
-    cout << "funciton3::you have provoded the interger:" << x << "\n";
 }
 
 int main()
