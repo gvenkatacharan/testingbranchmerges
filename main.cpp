3ce2aa889846210cde6f4deb7c72ee94740db291
gvenkatacharan <venkatacharan.g16@iiits.in>
2021-06-29 17:46:30 +0530

    removed both the functions

--- main.cpp
+++ main.cpp
@@ -1,19 +1,6 @@
 #include <stdio.h>
 
-void funciton(int x)
-{
-    cout << "funciton::you have provoded the interger:" << x << "\n";
-}
-
-
-void funciton2(int x)
-{
-    cout << "funciton::you have provoded the interger:" << x << "\n";
-}
-
 int main()
 {
     int x = 10;
-    funciton(x);
-    function2(x);
 }
